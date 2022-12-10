fc1de0fd14f11e4824dced960b950e0cf43cecd8
ali bizhangy <112679795+alibizhangy@users.noreply.github.com>
2022-12-10 13:33:57 +0330

    ادامه تمرین ها
    تمرین شماره ۵ باز نویسی شده 
    هنوز هم تکمیل نشده است

---
+++ 5.2.cpp
@@ -0,0 +1,60 @@
+// تابعی تعریف کنید که کلمات را از رشته ها جدا کند 
+// و انها را در خروجی چاپ کند 
+
+#include <iostream>
+#include <string>
+using namespace std ; 
+
+string  find_name (string str )
+{
+    int pos ,pos1 , pos2 ,length ;
+    int j ; 
+    int counter = 0 ; 
+    // this loop show you how much have word 
+    // and save the length on the counter 
+    for (int i = 0 ; i < str.length() ; i++) {
+        if (str[i]== " ") 
+        {
+            counter++ ; 
+        }
+    }
+    // and definitio array with lenght counter 
+    string  str_arr[counter++] ;
+
+
+    cout << "this string have " << ++counter << "name " << endl; 
+
+    // this loop find the word and save to one arr string 
+    for (int i = 0 ; i < str.length() ; i++) 
+    {
+        pos1 = pos2 ; 
+        if (str[i] == " ")
+        {
+            pos2 = i ; 
+            str.erase(pos2,1) ;
+        }
+        if (pos1 && pos2 )
+        {
+            pos = pos2 - pos1 ; 
+            str_arr[j].append(pos2,pos) ; 
+            j++ ;  
+        }
+
+
+    }
+    return str_arr; 
+}
+int main () 
+{
+    string end ; 
+    string name ; 
+    string find_name ;
+    name.append(" ");
+    while (end != "end") {
+        getline(cin,name);
+    }
+     std::copy(std::begin(name), std::end(name), std::begin(find_name(name)));
+    
+
+    return 0 ; 
+}
